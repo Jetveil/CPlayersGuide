8725cfee1a42801ab9440f8e05e26549ae013a0d
Pustotely <adresanetu@live.ru>
2025-04-25 17:47:05 +0300

    The thing namer 3000 in progress

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// --- What comes next
+
+// Console.WriteLine("Where am I?");
+// Console.WriteLine("How did i get here?");
+// Console.WriteLine("What's your name?");
+// Console.WriteLine("Who are you, guys?");
+// Console.WriteLine("Do you have guesses about Uncoded one?");
+
+// --- Consolas and Telim
+
+// Console.WriteLine("Bread is ready!");
+// Console.WriteLine("Who is the bread for?");
+// string name = Console.ReadLine();
+// if (name == null)
+// {
+//     Console.WriteLine("Incorrect name!");
+//     name = "Wanderer";
+// }
+// Console.WriteLine("Noted: " + name + " will get the bread");
+
+
+// --- The thing namer 3000
+
+Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine();
+string c = " of Doom";
+string d = "3000";
+Console.WriteLine("The " + b + " " + a + c + " " + d + "!");
