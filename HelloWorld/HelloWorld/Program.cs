ed7b7cdf86f315464720994fe7a481308f6a0380
Pustotely <adresanetu@live.ru>
2025-04-27 17:11:07 +0300

    Math chapter started

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -21,10 +21,42 @@
 
 // --- The thing namer 3000
 
-Console.WriteLine("What kind of thing are we talking about?");
-string a = Console.ReadLine(); /* This var is reading user input */
-Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
-string b = Console.ReadLine(); // This var is reading user input
-string c = " of Doom"; // This var is writing to the console
-string d = "3000"; /* This var is writing to the console */
-Console.WriteLine("The " + b + " " + a + c + " " + d + "!");
+// Console.WriteLine("What kind of thing are we talking about?");
+// string a = Console.ReadLine(); /* This var is reading user input */
+// Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+// string b = Console.ReadLine(); // This var is reading user input
+// string c = " of Doom"; // This var is writing to the console
+// string d = "3000"; /* This var is writing to the console */
+// Console.WriteLine("The " + b + " " + a + c + " " + d + "!");
+
+// --- The Variable Shop
+
+// byte aByte = 255;
+// short aShort = 32_767;
+// short bShort = -32_768;
+// int aInt = 2_147_483_647;
+// int bInt = -2_147_483_648;
+// long aLong = 9_223_372_036_854_775_807;
+// long bLong = -9_223_372_036_854_775_807;
+// sbyte aSByte = -127;
+// ushort aUShort = 65_535;
+// uint aUInt = 4_294_967_295;
+// ulong aULong = 18_446_744_073_709_551_615;
+// float aFloat = 3.4028235E+38F; // 10^38
+// double aDouble = 1.7976931348623157E+308; // 10^308
+// decimal aDecimal = 7.9228162514264337593543950335m; // 10^28
+//
+// Console.WriteLine(aByte);
+// Console.WriteLine(aShort);
+// Console.WriteLine(bShort);
+// Console.WriteLine(aInt);
+// Console.WriteLine(bInt);
+// Console.WriteLine(aLong);
+// Console.WriteLine(bLong);
+// Console.WriteLine(aSByte);
+// Console.WriteLine(aUShort);
+// Console.WriteLine(aUInt);
+// Console.WriteLine(aULong);
+// Console.WriteLine(aFloat);
+// Console.WriteLine(aDouble);
+// Console.WriteLine(aDecimal);
